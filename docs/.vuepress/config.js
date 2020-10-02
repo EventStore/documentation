c242a605f5610e34654945064966059434eaac8b
danjamescrosby <57628772+danjamescrosby@users.noreply.github.com>
2020-10-02 17:41:08 +0100

    Adds favicon links and tag manager to head

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,17 @@ module.exports = {
     dest: "public",
     title: "EventStoreDB Documentation",
     description: "The stream database built for Event Sourcing",
+    head: [
+      ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon.png"}],
+      ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
+      ['script', {}, `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WLD9W3L');
+      `]
+    ],
     plugins: [
         "@vuepress/active-header-links",
         "one-click-copy",
