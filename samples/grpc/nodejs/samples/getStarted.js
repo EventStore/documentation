4c79aee169e70c1fa216e426106778c8db36cdb0
Oskar Dudycz <oskar.dudycz@gmail.com>
2020-12-22 08:48:01 +0100

    Removed hardcoded connection string from getStarted.js samples

--- samples/grpc/nodejs/samples/getStarted.js
+++ samples/grpc/nodejs/samples/getStarted.js
@@ -9,10 +9,7 @@ import { v4 as uuid } from "uuid";
 export default function (router) {
   router.get("/", async function (req, res, next) {
     // #region createClient
-    //const client = EventStoreDBClient.connectionString("{connectionString}");
-    const client = EventStoreDBClient.connectionString(
-      "esdb://bv7mu9qrh41jl17k8ulg-0.mesdb.eventstore.cloud:2113,bv7mu9qrh41jl17k8ulg-1.mesdb.eventstore.cloud:2113,bv7mu9qrh41jl17k8ulg-2.mesdb.eventstore.cloud:2113"
-    );
+    const client = EventStoreDBClient.connectionString("{connectionString}");
     // #endregion createClient
 
     // #region createEvent
