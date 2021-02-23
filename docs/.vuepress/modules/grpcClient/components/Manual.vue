3466051d397cab4343414e037d6af1a9cb73b149
oskar.dudycz <oskar.dudycz@gmail.com>
2021-02-23 16:00:48 +0100

    Added setting up KeepAlive settings for gRPC client in connection wizard

--- docs/.vuepress/modules/grpcClient/components/Manual.vue
+++ docs/.vuepress/modules/grpcClient/components/Manual.vue
@@ -21,6 +21,17 @@
         </el-switch>
       </el-form-item>
 
+      <el-form-item label="KeepAlive:" prop="keepAlive.isEnabled">
+        <el-switch
+                v-model="keepAlive.isEnabled"
+                :disabled="disableKeepAlive"
+                active-text="Enabled"
+                inactive-text="Disabled">
+        </el-switch>
+      </el-form-item>
+
+      <KeepAlive ref="keepAliveForm" :keepAlive="keepAlive"/>
+
       <ClusterConnection/>
 
     </el-form>
@@ -30,16 +41,19 @@
 <script>
 import connection from "../domain/connection";
 import ClusterConnection from "./ClusterConnection";
+import KeepAlive from "./KeepAlive";
 import {isTrue} from "../../../lib/parse";
 
 export default {
     name:       "Manual",
-    components: {ClusterConnection},
+    components: {ClusterConnection, KeepAlive},
     computed:   {
-        state:           () => connection,
-        disableSecurity: () => connection.cloud,
-        cluster:         connection.extendedProperty("cluster", "changeTopology"),
-        secure:          connection.extendedProperty("secure", "changeSecurity"),
+        state:            () => connection,
+        disableSecurity:  () => connection.cloud,
+        disableKeepAlive: () => connection.cloud,
+        cluster:          connection.extendedProperty("cluster", "changeTopology"),
+        secure:           connection.extendedProperty("secure", "changeSecurity"),
+        keepAlive:        connection.extendedProperty("keepAlive", "changeKeepAlive")
     },
     mounted() {
         connection.changeTopology(isTrue(this.$route.query.cluster));
