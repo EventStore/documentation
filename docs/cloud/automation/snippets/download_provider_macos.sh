8ddb26aad69b3e03cad16f91ca54838b8672075a
Alexey Zimarev <alex@zimarev.com>
2021-03-15 17:48:54 +0100

    Terraform provider docs improvements

--- docs/cloud/automation/snippets/download_provider_macos.sh
+++ docs/cloud/automation/snippets/download_provider_macos.sh
@@ -1,3 +1,4 @@
-curl -o ./terraform-provider-eventstorecloud.zip -L https://github.com/EventStore/terraform-provider-eventstorecloud/releases/download/v1.5.1/terraform-provider-eventstorecloud_1.5.1_darwin_amd64.zip
+curl -o ./terraform-provider-eventstorecloud.zip -L \
+  https://github.com/EventStore/terraform-provider-eventstorecloud/releases/download/v1.5.1/terraform-provider-eventstorecloud_1.5.1_darwin_amd64.zip
 unzip ./terraform-provider-eventstorecloud.zip
-mv ./terraform-provider-eventstorecloud ~/.terraform.d/plugins/terraform-provider-eventstorecloud  
+mv ./terraform-provider-eventstorecloud ~/.terraform.d/plugins/terraform-provider-eventstorecloud
