628ecfb6e6dde1d1ec1a1047f6ae8523d2090556
oskar.dudycz <oskar.dudycz@gmail.com>
2021-03-16 16:27:49 +0100

    Added the Algolia scraper Config and GitHub Action

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -179,7 +179,7 @@ module.exports = {
         algolia: {
             apiKey: process.env.ALGOLIA_API_KEY,
             indexName: process.env.ALGOLIA_INDEX_NAME,
-            appId: process.env.ALGOLIA_APP_ID,
+            appId: process.env.ALGOLIA_APPLICATION_ID,
             hitsPerPage: 10,
           }
     },
