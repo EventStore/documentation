b57e7a732e806e551b29c32d7e4e4677a616d992
oskar.dudycz <oskar.dudycz@gmail.com>
2021-04-13 14:34:28 +0200

    Added entry in sidebar to display compatibility mode in the TCP client docs

--- docs/clients/dotnet/5.0/sidebar.js
+++ docs/clients/dotnet/5.0/sidebar.js
@@ -14,6 +14,7 @@ module.exports = [
             "connecting/",
             "connecting/options.md",
             "connecting/connection-string.md",
+            "connecting/compatibility-mode.md",
         ]
     },
     {
