708a22d1b278523ed0f0fd35a766ca3ff1a6c49f
Alexey Zimarev <alex@zimarev.com>
2021-06-30 19:47:06 +0200

    Add another reference

--- docs/.vuepress/theme/plugins/canonical/Canonical.vue
+++ docs/.vuepress/theme/plugins/canonical/Canonical.vue
@@ -3,8 +3,8 @@
 <script>
 export default {
     created() {
-        if (this.$ssrContext && this.$frontmatter && this.$frontmatter.original) {
-            this.$ssrContext.userHeadTags += `<link rel='canonical' href='${this.$frontmatter.original}'/>`;
+        if (this.$ssrContext && this.$page.frontmatter.original) {
+            this.$ssrContext.userHeadTags += `<link rel='canonical' href='${this.$page.frontmatter.original}'/>`;
         }
     }
 };
