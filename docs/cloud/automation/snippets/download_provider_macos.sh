62a30d460ab9eb3608e55340cbf85e95a09e5933
ylorph <ylorph@users.noreply.github.com>
2021-07-01 13:37:01 +0200

    bump terraform version from 1.5.3 to 1.5.4 (#388)

--- docs/cloud/automation/snippets/download_provider_macos.sh
+++ docs/cloud/automation/snippets/download_provider_macos.sh
@@ -1,4 +1,4 @@
 curl -o ./terraform-provider-eventstorecloud.zip -L \
-  https://github.com/EventStore/terraform-provider-eventstorecloud/releases/download/v1.5.3/terraform-provider-eventstorecloud_1.5.3_darwin_amd64.zip
+  https://github.com/EventStore/terraform-provider-eventstorecloud/releases/download/v1.5.4/terraform-provider-eventstorecloud_1.5.4_darwin_amd64.zip
 unzip ./terraform-provider-eventstorecloud.zip
 mv ./terraform-provider-eventstorecloud ~/.terraform.d/plugins/terraform-provider-eventstorecloud
