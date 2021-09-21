2d7b89eb9e2aa6d118c0809275602809c9e30efb
Alexey Zimarev <alex@zimarev.com>
2021-09-21 15:27:19 +0200

    Move out the deployment

---
+++ docs/.vuepress/components/dummy/Deployment.vue
@@ -0,0 +1,17 @@
+<template>
+  Try using the
+  <a href="https://quirky-einstein-c3abba.netlify.app/" target="_blank">
+    configuration tool
+  </a>
+  to generate server config files and certificates.
+</template>
+
+<script>
+export default {
+    name: "Deployment"
+}
+</script>
+
+<style scoped>
+
+</style>
