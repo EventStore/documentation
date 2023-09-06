0c0e324d2eb13c30e8e2d2e392cbb5376984cddb
Hayley Campbell <hayleyjeancampbell@gmail.com>
2023-09-06 12:40:14 +0200

    Allow for 'nightly' in version

--- docs/.vuepress/lib/version.ts
+++ docs/.vuepress/lib/version.ts
@@ -1,8 +1,12 @@
 const versionRegex = /v((\d+\.)?(\d+\.)?(\*|\d+))/;
+const nightly = "nightly";
 const v = {
     isVersion: (v: string) => versionRegex.test(v),
     parseVersion: (v: string) => versionRegex.exec(v),
     getVersion: (path: string): string | undefined => {
+        if (path.includes(nightly)) {
+            return nightly;
+        }
         const ref = path.split("#")[0];
         const split = ref.split("/");
         return split.find(x => v.isVersion(x));
