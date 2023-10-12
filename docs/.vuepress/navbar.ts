dfffaa66979fb00c64d3faba7b753e9912438201
Renae Sowald <renaesowald@gmail.com>
2023-10-11 17:57:53 -0700

    add theme and basic navbar and sidebar config

---
+++ docs/.vuepress/navbar.ts
@@ -0,0 +1,34 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  {
+    text: "Home",
+    link: "/",
+    icon: "home"
+  },
+  {
+    text: "Documentation",
+    link: "/documentation.md",
+    icon: "book-open"
+  },
+  {
+    text: "Samples",
+    link: "/samples/",
+    icon: "code"
+  },
+  {
+    text: "Tutorials",
+    link: "/tutorials/",
+    icon: "chalkboard"
+  },
+  {
+    text: "Articles",
+    link: "/resources/articles/",
+    icon: "newspaper"
+  },
+  {
+    text: "Videos",
+    link: "/resources/videos/",
+    icon: "play"
+  },
+]);
