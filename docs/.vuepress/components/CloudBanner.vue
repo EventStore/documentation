d0e50b755881b552deece070b3e54302577ac1ed
Alexey Zimarev <alex@zimarev.com>
2025-02-13 08:20:33 -0500

    Add a text

--- docs/.vuepress/components/CloudBanner.vue
+++ docs/.vuepress/components/CloudBanner.vue
@@ -1,11 +1,11 @@
 <template>
   <VPBanner
       title="Kurrent Cloud"
-      content="Spin up a managed instance of EventStoreDB and start building in minutes. Choose between AWS, Azure or GCP."
+      content="Spin up a managed instance of EventStoreDB and start building in minutes. Available in AWS, Azure or GCP."
       logo="/cloud.png"
       :actions='[
     {
-        text: "Sign up",
+        text: "Sign up for free",
         link:"https://www.kurrent.io/kurrent-cloud",
     },
     {
