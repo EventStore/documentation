a5b7e7f98856e0aabdfa34ebca98e7f661fc2464
Alexey Zimarev <alex@zimarev.com>
2025-03-25 22:42:48 +0100

    Use custom cdn

--- docs/.vuepress/lib/usePosthog.ts
+++ docs/.vuepress/lib/usePosthog.ts
@@ -3,7 +3,7 @@ import posthog from 'posthog-js';
 export function usePostHog() {
     try {
         posthog.init('phc_DeHBgHGersY4LmDlADnPrsCPOAmMO7QFOH8f4DVEVmD', {
-            api_host: 'https://eu.i.posthog.com'
+            api_host: 'https://phog.kurrent.io'
         });
     } catch (e) {
     }
